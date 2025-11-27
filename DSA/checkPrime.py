5a99b66de37c1d9f4b1f059c344f317e4b3593cb
Nguyen Cong Tuan <151908647+Congtuan168@users.noreply.github.com>
2025-11-27 10:39:09 +0700

    Update checkPrime.py

--- DSA/checkPrime.py
+++ DSA/checkPrime.py
@@ -1,4 +1,5 @@
 # Write a Python function that takes an integer as input and checks whether it is a prime number or not.
+# Ý tưởng: Dùng 1 vòng loop kiểm tra từ 3 => căn bậc 2 của inputNumber. Nếu tồn tại 1 số %2 == 0, thì inputNumber không phải Prime number; else thì số đó là prime 
 import math
 
 
@@ -6,11 +7,12 @@ def checkPrime():
     print(f'Enter a number')
     inputNumber = int(input())
 
-    # dùng for
     # check nho hon 2
     if inputNumber < 2:
         print(f"This is not a prime number")
         return False
+        
+    # dùng for
     # vì for chạy từ 2 đến 4 (bỏ mất 5) nên phải +1 vào
     # else:
     #     for i in range(2, int((math.sqrt(inputNumber) +1)) ):
